9c52be498d7a8a4d1faaeb3f6b1191d0ae3d77fb
Robert Zhang <ZiYang_Zhang@McAfee.com>
2015-03-17 23:58:55 -0400

    init project

---
+++ src/telecom/server/LeakyBucketSocket.java
@@ -0,0 +1,16 @@
+package telecom.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Created by robertzhang on 2015-03-17.
+ */
+public class LeakyBucketSocket extends ServerSocket {
+
+    public LeakyBucketSocket() throws IOException {
+        super();
+
+
+    }
+}
---
+++ src/telecom/server/Main.java
@@ -0,0 +1,8 @@
+package telecom.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
